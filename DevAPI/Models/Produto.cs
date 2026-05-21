7e020f393233913ccb88e9445816121986e44445
João Pedro <joaopedrogs248@gmail.com>
2026-05-20 22:46:46 -0300

    Add files via upload

---
+++ DevAPI/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace ApiProjeto.Models;
+
+public class Produto
+{
+
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
